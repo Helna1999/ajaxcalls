aa023db50d2d3151201e1140351c45927c14cb74
hskaria <hskaria@SUYCOKHWDSK128>
2022-01-17 14:34:02 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ajaxcalls.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+
+            return View();
+        }
+
+        public JsonResult Math(int nr1 , int nr2)
+        {
+            var result = nr1 + nr2;
+            /*var data = new { status = "ok", result = result };*/
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
